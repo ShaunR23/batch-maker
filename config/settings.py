a088a8a7d86abc4f3dbfe411116e29eecbcb771f
ShaunR23 <shaunradford23@gmail.com>
2022-02-28 15:54:57 -0500

    latest commit

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,9 @@ MEDIA_URL='/media/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'frontend/static/build/static'),)
 REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 SITE_ID = 1
 
