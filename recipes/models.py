393c3a263d639c3dc76c2492660e8e3bf8ab9b95
ShaunR23 <shaunradford23@gmail.com>
2022-06-06 14:30:33 -0400

    created recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+PHASES = (
+    ('DRAFT', 'Draft'),
+    ('SUBMITTED', 'Submitted'),
+)
+
+CATEGORY = (
+    ('POPULAR', 'Popular'),
+    ('FAVORITE', 'Favorite'),
+    ('ALL', 'All'),
+)
+
+RECIPE_TYPES = (
+    ('BREAKFAST', 'Breakfast'),
+    ('LUNCH', 'Lunch'),
+    ('DINNER', 'Dinner'),
+    ('DESSERT', 'Dessert'),
+)
+
+MEASUREMENTS = (
+    ('GRAMS', 'Grams'),
+    ('CUPS', 'Cups'),
+    ('OUNCES', 'Ounces'),
+    ('LBS', 'Pounds'),
+
+)
+
+class Recipe(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True)
+    recipe_type = models.CharField(max_length=255, choices=RECIPE_TYPES, default='DESSERT')
+    recipe_name = models.CharField(max_length=255, blank=True)
+    prep_time = models.IntegerField(default=0)
+    cook_time = models.IntegerField(default=0)
+    cook_temp = models.IntegerField(default=0)
+    yield_name = models.CharField(max_length=255, blank=True)
+    yield_quantity = models.IntegerField(default=0)
+    notes =models.CharField(max_length=255, blank=True)
+
+class Steps(models.Model):
+    step_number = models.IntegerField(default=1)
+    directions = models.CharField(max_length=255, blank=True)
+    measure_amount = models.IntegerField(default=0)
+    measure_type = models.CharField(max_length=255, choices=MEASUREMENTS, default='CUPS')
+    ingredient = models.CharField(max_length=255, default='Ingredient')
+
+
+
+
+
+
+
+
+
+
+
